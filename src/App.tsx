1f4bdb73f790bfd1cb4f7c22ef1595f465ecefbe
Jimmy Quach <jminquach@gmail.com>
2022-11-13 21:32:03 -0800

    add: RePlays to projects section

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import ColumnHeader from "./components/columnHeader"
 import win95 from './assets/win95-2.png'
-import { Github, Linkedin, FileEarmarkText } from 'react-bootstrap-icons';
+import replaysPreview from './assets/replays-preview.png'
+import { Github, Linkedin, FileEarmarkPdf } from 'react-bootstrap-icons';
 
 function App() {
   return (
@@ -22,6 +23,28 @@ function App() {
         <div className="text-stone-200">
           I'm a software developer & engineer working on building amazing applications for users in the future, 
           specializing in both web and desktop applications at the moment.
+        </div>
+      </div>
+{/*              */}
+{/* STICKY LINKS */}
+{/*              */}
+      <div className="items-left md:block md:w-4/6 lg:w-3/5 xl:w-2/5 md:mr-24 pb-6 md:pb-0">
+        <div className="flex md:flex-col md:fixed top-24 z-50 gap-2">
+          <a href="https://github.com/lulzsun" className="cursor-pointer group overflow-hidden bg-neutral-800 w-32 md:w-11 md:hover:w-28 h-11 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:bg-neutral-500 hover:drop-shadow-2xl duration-300">
+            <div className="md:fixed left-3.5 inline-flex items-center place-content-center">
+              <Github/> <span className="pl-3.5">GitHub</span>
+            </div>
+          </a>
+          <a href="https://www.linkedin.com/in/jminquach/" className="cursor-pointer group overflow-hidden bg-neutral-800 w-32 md:w-11 hover:w-32 h-11 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:bg-neutral-500 hover:drop-shadow-2xl duration-300">
+            <div className="md:fixed left-3.5 inline-flex items-center place-content-center">
+              <Linkedin/> <span className="pl-3.5">Linkedin</span>
+            </div>
+          </a>
+          <a className="cursor-pointer group overflow-hidden bg-neutral-800 w-32 md:w-11 hover:w-32 h-11 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:bg-neutral-500 hover:drop-shadow-2xl duration-300">
+            <div className="md:fixed left-3.5 inline-flex items-center place-content-center">
+              <FileEarmarkPdf/> <span className="pl-3.5">Resume</span>
+            </div>
+          </a>
         </div>
       </div>
 {/*          */}
@@ -49,7 +72,7 @@ function App() {
               </div>
             </div>
             <br/>
-            I would consider myself a self-taught developer, but I am currently pursuing a bachelors in computer science at the time of building this portfolio.
+            I would consider myself a self-taught developer, but I am currently pursuing a Bachelors in Computer Science at the time of building this portfolio.
           </div>
         </div>
         <div className="pt-4 flex flex-row">
@@ -73,24 +96,42 @@ function App() {
             Here are the technologies that I have been working with for software development:
           </div>
           <div className="grid grid-rows-3 grid-flow-col gap-3">
-            <div>• Javascript / Typescript</div>
-            <div>• Golang</div>
-            <div>• C# / .NET</div>
-
-            <div>• PostgreSQL</div>
-            <div>• MongoDB</div>
-            <div>• React</div>
+            <div className="flex"><span className="px-2">•</span><span>
+              Javascript / Typescript
+            </span></div>
+            <div className="flex"><span className="px-2">•</span><span>
+              Golang
+            </span></div>
+            <div className="flex"><span className="px-2">•</span><span>
+              C# / .NET
+            </span></div>
+            <div className="flex"><span className="px-2">•</span><span>
+              PostgreSQL
+            </span></div>
+            <div className="flex"><span className="px-2">•</span><span>
+              MongoDB
+            </span></div>
+            <div className="flex"><span className="px-2">•</span><span>
+              React
+            </span></div>
           </div>
           <div className="pb-4 text-stone-200">
             <br/>
             And if I'm not working with these technologies, then I might be tinkering in the <b>hardware</b> space (for fun)!
           </div>
           <div className="grid grid-rows-2 grid-flow-col gap-3">
-            <div>• PCB schematics / board design in KiCad</div>
-            <div>• 3D printing & modeling in Fusion360</div>
-
-            <div>• Soldering & hobby electrical work</div>
-            <div>• Microcontroller firmware (Arduino, RP2040)</div>
+            <div className="flex"><span className="px-2">•</span><span>
+              PCB schematics & board design in KiCad
+            </span></div>
+            <div className="flex"><span className="px-2">•</span><span>
+              3D printing & modeling in Fusion360
+            </span></div>
+            <div className="flex"><span className="px-2">•</span><span>
+              Soldering & hobby electrical work
+            </span></div>
+            <div className="flex"><span className="px-2">•</span><span>
+              Microcontroller firmware (Arduino, RP2040)
+            </span></div>
           </div>
         </div>
         <div className="pt-4 flex flex-row">
@@ -109,8 +150,16 @@ function App() {
             <span className="text-stone-400">/* Projects */</span>
             <span className="text-red-400 pl-1">&#125;</span>
           </div>
-          <div className="text-stone-200">
-            My proudest project which I have been continuously maintaining is RePlays.
+          <div>
+            <div className="text-stone-400 text-xl p-3">// RePlays</div>
+            <div className="relative pb-8">
+              <img className="rounded-lg drop-shadow-lg h-56" src={replaysPreview}/>
+              <div className="absolute left-1/2 bottom-0 rounded-xl drop-shadow-lg bg-stone-800 p-4">
+                <span>
+                  RePlays is a free and open source program that automatically manages recording of detected running games, with a clip editor that allows for quick video sharing.
+                </span>
+              </div>
+            </div>
           </div>
         </div>
         <div className="pt-4 flex flex-row">
@@ -118,27 +167,10 @@ function App() {
           <span className="text-red-400">;</span>
         </div>
       </div>
-{/*              */}
-{/* STICKY LINKS */}
-{/*              */}
-      <div className="items-left lg:block hidden md:block md:w-4/6 lg:w-3/5 xl:w-2/5 mr-24">
-        <div className="flex flex-col fixed top-24 z-90 gap-2">
-          <button className="group overflow-hidden bg-neutral-800 w-14 hover:w-32 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:bg-neutral-500 hover:drop-shadow-2xl duration-300">
-            <div className="fixed left-5 inline-flex items-center">
-              <Github/> <span className="pl-5">GitHub</span>
-            </div>
-          </button>
-          <button className="group overflow-hidden bg-neutral-800 w-14 hover:w-36 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:bg-neutral-500 hover:drop-shadow-2xl duration-300">
-            <div className="fixed left-5 inline-flex items-center">
-              <Linkedin/> <span className="pl-5">Linkedin</span>
-            </div>
-          </button>
-          <button className="group overflow-hidden bg-neutral-800 w-14 hover:w-36 h-14 rounded-full drop-shadow-lg flex justify-center items-center text-white hover:bg-neutral-500 hover:drop-shadow-2xl duration-300">
-            <div className="fixed left-5 inline-flex items-center">
-              <FileEarmarkText/> <span className="pl-5">Resume</span>
-            </div>
-          </button>
-        </div>
+{/*        */}
+{/* FOOTER */}
+{/*        */}
+      <div className="flex flex-col sm:w-3/4 md:w-4/6 lg:w-3/5 xl:w-2/5 p-6">
       </div>
     </div>
   )
