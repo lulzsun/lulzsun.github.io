0781ebc9112cb56cb18cf576daae4f3c8dbb2fbd
Jimmy Quach <jminquach@gmail.com>
2022-11-18 23:04:57 -0800

    feat: finish 1st revision of resume draft

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -17,12 +17,25 @@ const styles = StyleSheet.create({
   page: {
     flexDirection: 'column',
     margin: '1in',
+    marginTop: '0.85in',
   },
   header: {
     fontFamily: 'Petrona',
     alignContent: 'center',
+    textAlign: 'center',
     flexDirection: 'column',
     width: '6.3in',
+    marginBottom: '8pt'
+  },
+  contact: {
+    fontFamily: 'Petrona',
+    display: "flex", 
+    flexDirection: "row",
+    fontSize: '11pt',
+    width: '6.3in',
+    paddingLeft: '1.2in',
+    paddingRight: '1.2in',
+    marginBottom: '8pt'
   },
   heading: {
     fontFamily: 'Petrona',
@@ -37,17 +50,29 @@ const styles = StyleSheet.create({
     fontFamily: 'Petrona',
     fontSize: '11pt',
     flexDirection: 'column',
-    width: '6.3in',
+    width: '6.3in'
   }
 });
-  
+
 // Create Document Component
 const Resume = () => (
   <Document>
     <Page size="A4" style={styles.page}>
       <View style={styles.header}>
-        <Text>JIMMY QUACH</Text>
+        <Text style={{fontSize: '25pt', fontStyle: 'bold', borderBottomWidth: '1px', 
+          width: '6.3in', borderBottomColor: '#0', paddingBottom: '4pt'}}>
+          JIMMY QUACH
+        </Text>
       </View>
+      {/* <View style={{width: '6.3in', alignContent: 'center'}}> */}
+        <View style={styles.contact}>
+          <Text style={{textAlign: "center"}}>(714) 623-3495</Text>
+          <Text style={{flex: 1, textAlign: "center", fontSize: '20pt', fontStyle: 'bold', marginTop: '-5pt'}}>•</Text>
+          <Text style={{textAlign: "center"}}>Fullerton, CA</Text>
+          <Text style={{flex: 1, textAlign: "center", fontSize: '20pt', fontStyle: 'bold', marginTop: '-5pt'}}>•</Text>
+          <Text style={{textAlign: "center"}}>jminquach@gmail.com</Text>
+        </View>
+      {/* </View> */}
       <View style={styles.heading}>
         <Text>EDUCATION</Text>
       </View>
@@ -57,16 +82,29 @@ const Resume = () => (
           <Text style={{textAlign: "right"}}>Fullerton, CA</Text>
         </View>
         <View style={{paddingBottom: '4pt', flexDirection: 'row'}}>
-          <Text style={{flex: 1}}>BS Computer Science</Text>
+          <Text style={{flex: 1}}>Bachelor of Science In Computer Science</Text>
           <Text style={{textAlign: "right"}}>Expected: Aug. 2024</Text>
         </View>o
         <View style={{paddingBottom: '2pt', flexDirection: 'row'}}>
           <Text style={{flex: 1, fontStyle: 'bold'}}>Fullerton College</Text>
           <Text style={{textAlign: "right"}}>Fullerton, CA</Text>
         </View>
-        <View style={{paddingBottom: '8pt', flexDirection: 'row'}}>
-          <Text style={{flex: 1}}>AS Computer Science</Text>
+        <View style={{paddingBottom: '4pt', flexDirection: 'row'}}>
+          <Text style={{flex: 1}}>Associate of Science In Computer Science</Text>
           <Text style={{textAlign: "right"}}>May 2022</Text>
+        </View>
+        <View style={{paddingBottom: '2pt', flexDirection: 'row'}}>
+          <Text style={{fontStyle: 'bold'}}>Noteworthy Courses: </Text>
+        </View>
+        <View style={{paddingBottom: '8pt', flexDirection: 'row'}}>
+          <Text style={{flex: 1}}>
+            Algorithm Engineering,
+            Computer Organization & Assembly Language Programming,
+            Compilers and Languages,
+            File Structure & Database, 
+            Java Programming,
+            Software Engineering
+          </Text>
         </View>
       </View>
 
@@ -76,11 +114,11 @@ const Resume = () => (
       <View style={styles.section}>
         <View style={{paddingBottom: '5pt', width: '6.3in', flexDirection: 'row'}}>
           <Text style={{flex: "0.4pt"}}>Programming Languages:</Text>
-          <Text style={{flex: 1}}>C#, Javascript, Typescript, Go, SQL</Text>
+          <Text style={{flex: 1}}>C#, Go, Javascript, Typescript, SQL</Text>
         </View>
         <View style={{paddingBottom: '5pt', width: '6.3in', flexDirection: 'row'}}>
           <Text style={{flex: "0.4pt"}}>Markup Languages:</Text>
-          <Text style={{flex: 1}}>HTML, CSS, JSON, XML</Text>
+          <Text style={{flex: 1}}>CSS, HTML, JSON, XML</Text>
         </View>
         <View style={{paddingBottom: '5pt', width: '6.3in', flexDirection: 'row'}}>
           <Text style={{flex: "0.4pt"}}>Frameworks/Libraries:</Text>
@@ -102,17 +140,17 @@ const Resume = () => (
         </View>
         <View style={{paddingBottom: '4pt', flexDirection: 'row'}}>
           <Text style={{flex: 1}}>A desktop application that automatically 
-            manages recording of running video games, with a clip editor 
-            that allows for quick video sharing.
+            screen captures full recordings of detected video games on the system, 
+            with a built-in clip editor that allows for quick video editing and sharing.
           </Text>
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
-          <Text style={{flex: 1, marginTop: '6pt'}}>Utilization of GitHub's CI/CD pipeline to automatically generate builds</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Utilization of GitHub's CI/CD pipeline to automatically generate builds on conditions</Text>
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
-          <Text style={{flex: 1, marginTop: '6pt'}}>Desktop application back-end powered by .NET Core, written in C#</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Desktop application back-end powered by .NET Core/C# and is cross-platform capable</Text>
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
@@ -120,11 +158,11 @@ const Resume = () => (
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
-          <Text style={{flex: 1, marginTop: '6pt'}}>Developed assisting C# wrapper around C++ library to help in specific functionality</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Developed assisting C# wrapper around C++ library to hook into low level functions</Text>
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
-          <Text style={{flex: 1, marginTop: '6pt'}}>Manage collaborators who assist in development, by providing code reviews</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Managed open-source collaborators who assist in development by doing code reviews</Text>
         </View>
       </View>
       <View style={styles.section}>
@@ -134,8 +172,8 @@ const Resume = () => (
         </View>
         <View style={{paddingBottom: '4pt', flexDirection: 'row'}}>
           <Text style={{flex: 1}}>A reimplementation of an open source library called
-            geckos.io written in Go, which allows for real-time UDP client/server 
-            network communication using WebRTC.
+            Geckos.io written in Go, allowing for performant, real-time, client-server model
+            UDP network communication using WebRTC.
           </Text>
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
@@ -144,7 +182,11 @@ const Resume = () => (
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
-          <Text style={{flex: 1, marginTop: '6pt'}}>Developed using event-driven architecture design patterns</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Developed using event-driven architecture for ease of use with future developers</Text>
+        </View>
+        <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
+          <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Designed with a concurrent design pattern for efficient use of I/O and multiple CPUs</Text>
         </View>
       </View>
       <View style={styles.section}>
@@ -153,20 +195,42 @@ const Resume = () => (
           <Text style={{textAlign: "right"}}>In Progress</Text>
         </View>
         <View style={{paddingBottom: '4pt', flexDirection: 'row'}}>
-          <Text style={{flex: 1}}>A social media platform in which users interact with each other in 3-Dimensional virtual spaces with virtual avatars.
+          <Text style={{flex: 1}}>
+            A social media platform in which users interact with each other in 3-Dimensional virtual spaces with virtual avatars.
+            Heavily inspired by Metaverse & VRChat's virtual realities.
           </Text>
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
-          <Text style={{flex: 1, marginTop: '6pt'}}>Using Supabase, an open source Firebase alternative for managing SQL database</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Using Supabase, an open-source Firebase alternative for managing a SQL database</Text>
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
-          <Text style={{flex: 1, marginTop: '6pt'}}>Front-end developed with React, Next.js & Three.js to render 3D models and environments</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Developed with React and Next.js frameworks, allowing for fast server-side rendering</Text>
         </View>
         <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
           <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
-          <Text style={{flex: 1, marginTop: '6pt'}}>Custom back-end written in Go to sync avatar movements using UDP network communication</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Designed with TailwindCSS & Mantine UI, with Three.js to render 3D environments</Text>
+        </View>
+        <View style={{paddingLeft: '32pt', marginTop: '-6pt', flexDirection: 'row'}}>
+          <Text style={{flex: "0.04pt", fontSize: '20pt', fontStyle: 'bold', marginLeft: '-16pt'}}>•</Text>
+          <Text style={{flex: 1, marginTop: '6pt'}}>Back-end written in Go to sync player movements using UDP network communication</Text>
+        </View>
+      </View>
+
+      <View style={styles.heading}>
+        <Text>PERSONAL INTERESTS</Text>
+      </View>
+      <View style={styles.section}>
+        <View style={{paddingBottom: '5pt', width: '6.3in', flexDirection: 'row'}}>
+          <Text>
+            3D printing, 
+            Electronics & soldering, 
+            Hardware development (PCB Schematics & Design), 
+            Self-hosted web services,
+            Video games (League of Legends, Tekken),
+            Virtualization (Proxmox)
+          </Text>
         </View>
       </View>
     </Page>
