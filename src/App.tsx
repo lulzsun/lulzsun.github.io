b5bfc6109199489fd1468556b62f7e784db83f0d
Jimmy Quach <jminquach@gmail.com>
2023-02-12 10:55:31 -0800

    fix: gh-pages build folder location

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Hello, world!
         </p>
       </div>
       <p className="read-the-docs">
