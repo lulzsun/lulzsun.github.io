99c19af847556a0758d5ffd95a66931efa23b72c
Jimmy Quach <jminquach@gmail.com>
2023-03-28 12:26:44 -0700

    Updates

--- assets/src/pages/blog/@post/posts/thinkpad-x330/index.a518b833.js
+++
@@ -1,4 +0,0 @@
-import{R as a,M as t,p as n,q as r}from"../../../../../../chunks/7cfff31d.js";import{a as e,F as o}from"../../../../../../chunks/fd325986.js";import"../../../../../../chunks/53706c28.js";import"../../../../../../chunks/8f284688.js";const p={title:"ThinkPad x230 (x330) | Jimmy Quach",description:"My documented adventure where I mod my personal laptop.",tags:["hardware","modding"],createdAt:new Date("2023-3-27")};function l(){return e(o,{children:e("div",{className:"w-5/6 sm:w-3/4 md:w-4/6 lg:w-3/5 xl:w-2/5 flex flex-col gap-6",children:e(a,{components:t,rehypePlugins:[n],children:r`
-        # ThinkPad x230 (x330)
-        Blog to be written...
-      `})})})}export{l as Page,p as metaData};
