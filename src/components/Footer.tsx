90956e3f9763ee384145d514227dcbfd23e5d9d7
Jimmy Quach <jminquach@gmail.com>
2023-05-04 19:44:34 -0700

    add blog redirect to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Envelope, FileEarmarkPdf, Github, Linkedin } from "react-bootstrap-icons";
+import { ChatLeftQuoteFill, Envelope, FileEarmarkPdf, Github, Linkedin } from "react-bootstrap-icons";
 
 interface Props {
   openResume: () => void,
@@ -25,17 +25,32 @@ export const Footer: React.FC<Props> = ({openResume}) => {
         </div>
       </a>
       <span className="hidden md:block">•</span>
+      <a className="hidden md:block hover:underline underline-offset-2 hover:text-red-300" href="/blog">
+        <div className="inline-flex items-center place-content-center">
+          <ChatLeftQuoteFill/> <span className="pl-3.5 whitespace-nowrap">Blog</span>
+        </div>
+      </a>
+      <span className="hidden md:block">•</span>
       <a className="hidden md:block hover:underline underline-offset-2 hover:text-red-300" href="mailto:jminquach@gmail.com">
         <div className="inline-flex items-center place-content-center">
           <Envelope/> <span className="pl-3.5 whitespace-nowrap">Contact Me</span>
         </div>
       </a>
     </div>
-    <a className="pb-16 pt-3 md:hidden block hover:underline underline-offset-2 hover:text-red-300" href="mailto:jminquach@gmail.com">
-      <div className="inline-flex items-center place-content-center">
-        <Envelope/> <span className="pl-3.5 whitespace-nowrap">Contact Me</span>
-      </div>
-    </a>
+    {/* for responsive mobile layout... there's probably a better way to do this but i am lazy */}
+    <div className="md:hidden flex items-center gap-6">
+      <a className="pb-16 pt-3 hover:underline underline-offset-2 hover:text-red-300" href="/blog">
+        <div className="inline-flex items-center place-content-center">
+          <ChatLeftQuoteFill/> <span className="pl-3.5 whitespace-nowrap">Blog</span>
+        </div>
+      </a>
+      <span className="pb-16 pt-3">•</span>
+      <a className="pb-16 pt-3 md:hidden block hover:underline underline-offset-2 hover:text-red-300" href="mailto:jminquach@gmail.com">
+        <div className="inline-flex items-center place-content-center">
+          <Envelope/> <span className="pl-3.5 whitespace-nowrap">Contact Me</span>
+        </div>
+      </a>
+    </div>
   </>
   )
 }
