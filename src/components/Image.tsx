1cf398b608cd12f9271a332a22f63e3bb64b24c9
Jimmy Quach <jminquach@gmail.com>
2023-05-23 07:59:09 -0700

    style: minor code style change

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -3,7 +3,7 @@ import { DetailedHTMLProps } from "react";
 export const Image: React.FC<DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>,HTMLImageElement>> = 
 (props: DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>,HTMLImageElement>) => {
   return (
-    <img {...props} src={!props.src?.includes(".svg") && process.env.NODE_ENV !== "development" ? props.src + ".webp" : props.src}/> 
+    <img {...props} src={(!props.src?.includes(".svg") && process.env.NODE_ENV !== "development") ? props.src + ".webp" : props.src}/> 
   )
 }
   
