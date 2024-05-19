92539741c056db9c50833eb90bc23e01651369c4
lulzsun <lulzsun@users.noreply.github.com>
2024-05-19 03:50:07 +0000

    deploy: 4983f65e8eb3db2502932acec25d2017d9adf427

--- server/importBuild.cjs
+++
@@ -1,8 +0,0 @@
-{
-  const { setLoaders } = require('../../node_modules/vite-plugin-ssr/dist/cjs/node/plugin/plugins/importBuild/loadBuild.js');
-  setLoaders({
-    pageFiles: () => import('./pageFiles.mjs'),
-    clientManifest: () => require('../client/manifest.json'),
-    pluginManifest: () => require('../client/vite-plugin-ssr.json'),
-  });
-}
