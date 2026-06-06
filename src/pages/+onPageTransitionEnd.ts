7634ec5a3f33c8c26f9cbbb79a8c741f00838c44
Jimmy Quach <jminquach@gmail.com>
2026-06-06 13:41:18 -0700

    feat: change client scroll behavior

--- src/pages/+onPageTransitionEnd.ts
+++ src/pages/+onPageTransitionEnd.ts
@@ -1,4 +1,9 @@
+import { scrollPositions } from "./+onPageTransitionStart";
+
 export async function onPageTransitionEnd() {
   console.log("Page transition end");
   document.body.classList.remove("page-transition");
+  const saved = scrollPositions.get(window.location.pathname);
+  console.log(saved);
+  window.scrollTo(0, saved ?? 0);
 }
--- src/pages/+onPageTransitionStart.ts
+++ src/pages/+onPageTransitionStart.ts
@@ -1,9 +1,11 @@
 // https://vike.dev/onPageTransitionStart
 
 import type { PageContextClient } from "vike/types";
+export const scrollPositions = new Map<string, number>();
 
 export async function onPageTransitionStart(pageContext: Partial<PageContextClient>) {
   console.log("Page transition start");
   console.log("pageContext.isBackwardNavigation", pageContext.isBackwardNavigation);
   document.body.classList.add("page-transition");
+  scrollPositions.set(window.location.pathname, window.scrollY);
 }
