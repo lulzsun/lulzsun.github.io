866260511c43a835a728545830ab69619b060e8b
Jimmy Quach <jminquach@gmail.com>
2026-06-05 23:17:22 -0700

    fix: broken images

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -2,9 +2,17 @@ import { DetailedHTMLProps } from "react";
 
 export const Image: React.FC<DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>,HTMLImageElement>> = 
 (props: DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>,HTMLImageElement>) => {
+  const src = props.src;
+  const webpSrc = src?.replace(/\.(png|jpe?g|gif)$/i, '.webp');
+  
   return (
-    <img {...props} src={(!props.src?.includes(".svg") && process.env.NODE_ENV !== "development") ? props.src + ".webp" : props.src}/> 
-  )
-}
+    <img
+      {...props}
+      src={(!src?.includes('.svg') && !import.meta.env.DEV)
+        ? webpSrc
+        : src}
+    />
+  );
+};
   
 export default Image;
