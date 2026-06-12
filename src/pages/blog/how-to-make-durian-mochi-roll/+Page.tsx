3d4519962a9d4751e169c5134314a42d637472a5
Jimmy Quach <jminquach@gmail.com>
2026-06-12 01:09:05 -0700

    feat: update mochi recipe

--- src/pages/blog/how-to-make-durian-mochi-roll/+Page.tsx
+++ src/pages/blog/how-to-make-durian-mochi-roll/+Page.tsx
@@ -24,8 +24,6 @@ export default function Page() {
           rehypePlugins={[rehypeRaw]}
           children={dedent`
             # How to Make Durian Mochi Rolls
-            THIS RECIPE IS IN EXPERIMENTAL PHASE. DO NOT MAKE UNTIL THIS NOTICE IS REMOVED.
-
             The inspiration for this recipe is a dessert from [Phoenix Food and Dessert](https://phoenixfood.us).
 
             Don't like durian or having difficulty obtaining it? Substitute it with mango!
@@ -41,17 +39,17 @@ export default function Page() {
             <div className="flex-1 border-t border-base-content/20" />
           </div>
           <span className="text-xl font-medium whitespace-nowrap">Mochi Dough</span>
-          <IngredientCheckBox>200g Glutinous rice flour</IngredientCheckBox>
-          <IngredientCheckBox>20g Rice flour</IngredientCheckBox>
-          <IngredientCheckBox>20g Corn (or Wheat) starch</IngredientCheckBox>
-          <IngredientCheckBox>20g Neutral vegetable oil (Corn/Soybean oil)</IngredientCheckBox>
-          <IngredientCheckBox>350g Warm water</IngredientCheckBox>
-          <IngredientCheckBox>35g White sugar</IngredientCheckBox>
+          <IngredientCheckBox>100g Glutinous rice flour</IngredientCheckBox>
+          <IngredientCheckBox>10g Rice flour</IngredientCheckBox>
+          <IngredientCheckBox>10g Corn (or Wheat) starch</IngredientCheckBox>
+          <IngredientCheckBox>10g Neutral vegetable oil (Corn/Soybean oil)</IngredientCheckBox>
+          <IngredientCheckBox>175g Warm water</IngredientCheckBox>
+          <IngredientCheckBox>17.5g White sugar</IngredientCheckBox>
           <IngredientCheckBox>Fine shredded coconut (optional, for coating the roll)</IngredientCheckBox>
           <span className="text-xl font-medium whitespace-nowrap">Durian Cream Filling</span>
           <IngredientCheckBox>200g Fresh/Frozen durian flesh (can sub for mango here)</IngredientCheckBox>
-          <IngredientCheckBox>254g (250ml) Heavy Cream</IngredientCheckBox>
-          <IngredientCheckBox>30g White sugar</IngredientCheckBox>
+          <IngredientCheckBox>85g (82ml) Heavy Cream</IngredientCheckBox>
+          <IngredientCheckBox>10g White sugar</IngredientCheckBox>
           <div className="flex items-center gap-3 w-full my-4">
             <span className="text-2xl font-medium whitespace-nowrap">Instructions</span>
             <div className="flex-1 border-t border-base-content/20" />
@@ -80,12 +78,14 @@ export default function Page() {
               5. Steam for 30 minutes on high. After 30 minutes, set aside to cool.
               ### Durian Cream Filling
               1. In another mixing bowl, whip heavy cream and sugar to stiff peaks using electric mixer.
-              2. Gently fold in 2/3 durian flesh diced (save the 1/3 undiced flesh) until fully incorporated.
-              3. Spread the filling onto cling wrap and wrap it up in a roll. Freeze for about 30 minutes or until firm enough to handle.
+              2. Gently fold in 1/2 mashed durian flesh until fully incorporated.
+              3. Put cream filling in freezer for about 30 minutes or until slighty firm enough to handle.
               ### Assembly
-              1. Dust work surface with shredded coconut flakes (and/or cornstarch). Flatten out mochi dough to roll like sushi.
-              2. Place filling roll and the saved 1/3 fresh/defrosted durian flesh in the middle of the dough.
-              3. Connect one edge of the mochi dough to the other end and form the roll.
+              1. Spread out cling wrap onto work surface for rolling. 
+              2. Lightly dust work surface with shredded coconut flakes (and/or cornstarch) and spread out the mochi dough onto work surface.
+              3. Layer a second sheet of cling wrap on top of the mochi (sandwiching it), and start rolling out the dough to a desired thickness (maybe about 4mm in thickness)
+              4. Scoop filling into roll and the saved 1/2 fresh/defrosted durian flesh in the middle of the dough.
+              5. Connect one edge of the mochi dough to the other end and form the roll.
             `}
           />
         </div>
